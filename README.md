b6cd1162b795479031016cc997846aa54c6b7a62
unknown <pak.phil7@gmail.com>
2020-04-12 14:15:48 -0700

    readme generator now generates a readme using title and description answers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# README-Generator
+##README Generator
+Make your README file look professional. The README generator will ask you several questions and will generate a README file for your repository at the end.
--- index.js
+++ index.js
@@ -1,50 +1,73 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
 inquirer.prompt([
     {
         type: "input",
-        name: "name",
-        message: "What is your GitHub Username?"
+        name: "username",
+        message: "Enter your GitHub Username: ",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email?"
+        message: "Enter your email: ",
     },
     {
         type: "input",
         name: "title",
-        message: "What is the title of your repository?"
+        message: "Enter the title of your repository: ",
     },
     {
         type: "input",
         name: "description",
-        message: "What is your repository's description?"
+        message: "Enter the description of your repository: ",
     },
     {
         type: "input",
         name: "installation",
-        message: "What are the steps to install your project?"
+        message: "What are the steps to install your project?",
     },
     {
         type: "input",
         name: "usage",
-        message: "How do we use your project?"
+        message: "How do we use your project?",
     },
     {
         type: "input",
         name: "license",
-        message: "What license is associated with your repository?"
+        message: "Enter any license associated with your repository: ",
     },
     {
         type: "input",
         name: "contributing",
-        message: "List any collaborators and links to their github profiles. List the creators of any third-party assets that you used. Also, include links to any tutorials that you may have followed."
+        message: "List any collaborators and links to their github profiles: ",
     },
     {
         type: "input",
         name: "test",
-        message: "Please include any tests here with examples on how to run them."
+        message: "Enter any tests here with examples on how to run them: ",
     },
-])
+
+]).then(answers => {
+    //Obtains the user's github page
+    const queryUrl = "https://api.github.com/users/" + answers.username;
+
+    axios.get(queryUrl).then(function(res) {
+        const userPage = res.data.html_url
+        console.log (userPage)
+    });
+
+    //Creates a ReadMe file with the title
+    fs.writeFile("README.md", "##" + answers.title + "\n", function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+    fs.appendFile("README.md", answers.description + "\n", function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+})
